4f8b0507f877a657dc0dc3bc035e74d4a7608e1e
jmcarcell <jmcarcell@users.noreply.github.com>
2024-03-22 16:48:47 +0100

    Update the push script

--- Docker/ubuntu22/push.sh
+++ Docker/ubuntu22/push.sh
@@ -1,3 +1,5 @@
+docker build -t gitlab-registry.cern.ch/key4hep/k4-deploy/ubuntu22 ubuntu22-build
+docker build -t gitlab-registry.cern.ch/key4hep/k4-deploy/ubuntu22-cvmfs ubuntu22-cvmfs
 docker push gitlab-registry.cern.ch/key4hep/k4-deploy/ubuntu22
 docker push gitlab-registry.cern.ch/key4hep/k4-deploy/ubuntu22-cvmfs
 
