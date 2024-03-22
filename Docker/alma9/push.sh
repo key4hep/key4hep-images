a4d39e64810e83b57dfeab53fa5cac8a93d0e967
jmcarcell <jmcarcell@users.noreply.github.com>
2024-03-22 16:47:28 +0100

    Update the push script

--- Docker/alma9/push.sh
+++ Docker/alma9/push.sh
@@ -1,3 +1,5 @@
+docker build -t gitlab-registry.cern.ch/key4hep/k4-deploy/alma9 alma9
+docker build -t gitlab-registry.cern.ch/key4hep/k4-deploy/alma9-cvmfs alma9-cvmfs
 docker push gitlab-registry.cern.ch/key4hep/k4-deploy/alma9
 docker push gitlab-registry.cern.ch/key4hep/k4-deploy/alma9-cvmfs
 
