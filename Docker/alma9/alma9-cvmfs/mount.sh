cae04238c5fd216a8428d008081a564abbd68d6b
jmcarcell <jmcarcell@users.noreply.github.com>
2024-05-24 08:49:38 +0200

    Update images

--- Docker/alma9/alma9-cvmfs/mount.sh
+++ Docker/alma9/alma9-cvmfs/mount.sh
@@ -1,3 +1,8 @@
+if mount | grep -q cvmfs; then
+  echo "CVMFS already mounted"
+  echo "/mount.sh does not need to be run anymore"
+  exit 0
+fi
 set -e
 mount -t cvmfs cvmfs-config.cern.ch /cvmfs/cvmfs-config.cern.ch
 mount -t cvmfs sw.hsf.org /cvmfs/sw.hsf.org
