fd3e979f671ed597817a35dbd8ffffd98d71a740
Federico Meloni <madb4ron@gmail.com>
2025-08-10 23:14:07 +0200

    removed unused scripts

--- Docker/alma9/push.sh
+++
@@ -1,5 +0,0 @@
-docker tag alma9 gitlab-registry.cern.ch/key4hep/k4-deploy/alma9
-docker tag alma9-cvmfs gitlab-registry.cern.ch/key4hep/k4-deploy/alma9-cvmfs
-docker push gitlab-registry.cern.ch/key4hep/k4-deploy/alma9
-docker push gitlab-registry.cern.ch/key4hep/k4-deploy/alma9-cvmfs
-
--- Docker/ubuntu24/push.sh
+++
@@ -1,5 +0,0 @@
-docker tag ubuntu24 gitlab-registry.cern.ch/key4hep/k4-deploy/ubuntu24
-docker tag ubuntu24-cvmfs gitlab-registry.cern.ch/key4hep/k4-deploy/ubuntu24-cvmfs
-docker push gitlab-registry.cern.ch/key4hep/k4-deploy/ubuntu24
-docker push gitlab-registry.cern.ch/key4hep/k4-deploy/ubuntu24-cvmfs
-
